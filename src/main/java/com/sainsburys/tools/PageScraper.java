86aefd52e0ed3d77f2eea11da0501f4d23f55944
tech <tech@techs-MacBook-Pro.local>
2017-09-24 19:26:24 +0100

    get page source and parse items

---
+++ src/main/java/com/sainsburys/tools/PageScraper.java
@@ -0,0 +1,7 @@
+package com.sainsburys.tools;
+
+/**
+ * Created by tech on 24/09/2017.
+ */
+public class PageScraper {
+}
---
+++ src/test/java/com/sainsburys/tools/PageScraperTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by tech on 24/09/2017.
+ */
+public class PageScraperTest {
+
+}
